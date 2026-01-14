e334a52836b9f62edf40a1e0a1c230b27a091a13
Seshiro_Nagi <faisalalsalkini@gmail.com>
2026-01-14 16:09:03 +0300

    firebase installation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:social_hub/app/my_app.dart';
+import 'package:social_hub/firebase_options.dart';
 
-void main() {
+void main() async {
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    // ignore: avoid_print
+    print(e);
+  }
   runApp(MyApp());
 }
